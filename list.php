7c04879e77df0da4859dbdeec9ebd1ff1adf18e5
buxuku <buxuku@gmail.com>
2016-02-25 17:08:03 +0800

    a simple guestbok

--- list.php
+++ list.php
@@ -1,11 +1,11 @@
 <?php
 $f = fopen("data.txt", "r");
 $gid = $_GET['tid'];
-	$tid = 1;
+$tid = 1;
 while (($row = fgetcsv($f)) != false) {
-		if($tid == $gid || !$gid){
+	if($tid == $gid || !$gid){
 		echo "<a href='?tid=".$tid."'>标题:".$row[0]."</a><br/>";
 		echo "内容:".$row[1]."<br/>";
-		}
-		$tid++;
 	}
+	$tid++;
+}
